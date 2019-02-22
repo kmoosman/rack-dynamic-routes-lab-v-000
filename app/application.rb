41760a24d5732433d54831c53a86d9d9af20475f
Katie Moosman <moosman.katie@gmail.com>
2019-02-22 22:13:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
         item = req.path.split("/items/").last
-        if item =@@items.find{|i| i.name == item}
+        if item = @@items.find{|i| i.name == item}
           resp.write item.price
         else
           resp.status = 400
